7c56ac37c6174fcb428483b30376997a3fc678d7
oceguemma <113361856+E-O-Conchas@users.noreply.github.com>
2024-11-12 10:20:15 +0100

    Function to import several folder to GRASS GIS created
    This function can be used to import several folders into GRASS GIS

--- 01.load_vrt_to_grass.py
+++ 01.load_vrt_to_grass.py
@@ -1,16 +1,57 @@
-# This script is intended to be used within the GRASS GIS environment.
+#######################################################################
+# Author: Emmanuel Oceguera-Conchas, Nikolaij
+# Work: Import multiple TIFF files into GRASS GIS
+#
+# Note: This script is intended to be used within the GRASS GIS environment.
+#######################################################################
 
+# Import libraries
+import os
+from glob import glob
 from grass.pygrass.modules import Module
 
-# Path to your Tiff file
-input_raster = "S:/Path_to_your_data_set/fragmentation/.tiff"
+# Input folder
+input_raster = "S:/Path_to_your_data_set/fragmentation"
 
-# Define the name of the output raster layer in GRASS GIS
-output_raster = "raster_layer_name"
+# Define a function to import all TIFF files in the folder
+def import_tiff_from_folders(folders, overwrite=True):
+    '''
+    Imports all TIFF files from a list of folders into GRASS GIS.
 
-# Import the raster using r.in.gdal
-Module("r.in.gdal", input=input_raster, output=output_raster, overwrite=True)
+    parameters:
+    Folder(list): List of folder paths containing the TIFF files.
+    overwrite(bool): whether to evrwrite existing layers in GRASS GIS.
 
+    '''
+    for folder in folders:
+
+        # Find all the TIFF in the current folder
+        tiff_files = glob(os.path.join(folder, "*.tiff"))
+
+        if not tiff_files:
+            print(f"No TIFF files found in {folder}.")
+            continue
+
+        for tiff_file in tiff_files:
+
+            # Define the output raster name based on the file name
+            output_raster = os.path.basename(tiff_file).replace('.tif', '')
+
+            # Import raster using r.in.gdal
+            print(f"Importing {tiff_file} as {output_raster}...")
+            Module("r.in.gdal", input=tiff_file, output=output_raster, overwrite=overwrite)
+            print(f"Imported {tiff_file} successfully.")
+
+
+# List of folders containing TIFF files
+folders = [
+    "S:/Path_to_your_data_set/fragmentation",
+    "S:/Path_to_your_data_set/fragmentation",
+    "S:/Path_to_your_data_set/fragmentation",
+]
+
+# Call the function
+import_tiff_from_folders(folders)
 
 # Note: The other way to upload a raster is as follows 
  # 1. Click the File button on the toolbar. 
