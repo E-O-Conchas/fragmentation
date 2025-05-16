b08c2227ea9229b264a9583faf7d35bb172c2877
E-O-Conchas <e.oceguera@idiv.de>
2025-05-16 13:43:09 +0200

    Implement parallel processing for habitat analysis using future.apply package

--- R/mask_idicator_results_eu_mask.R
+++ R/mask_idicator_results_eu_mask.R
@@ -77,4 +77,79 @@ for (group in habitats_groups[2]) {
     
   }
 }
+
+
+
+
+#### We parallelize the code ####
+# We use the future package to parallelize the code
+# It works with the future.apply package and the terra package
+# It is way faster than the previous code
+
+# Clean the environment
+rm(list = ls())
+gc()
+
+# Load libraries
+library(terra)
+library(future.apply)
+
+# Parallel setup
+plan(multisession, workers = 4)  # Adjust number of workers based on your system
+
+# Define paths
+root <- "D:\\Emmanuel_Oceguera\\Fragmentation_analysis\\output\\EUNIS"
+eu_mask_path <- "I:/biocon/Emmanuel_Oceguera/projects/Fragmentation_analysis/data/eu_mask_single.shp"
+output_root <- "S:\\Emmanuel_OcegueraConchas\\fragmentation_analysis\\test"
+year <- 2012
+#eu_mask <- terra::vect(eu_mask_path)
+habitats_groups <- c('N', 'Q', 'R', 'S', 'T', 'U', 'V')
+
+# Loop over groups (sequentially, but can also be parallelized)
+for (group in habitats_groups[2]) {
+  cat("\nProcessing group:", group, "\n")
+  group_dir <- file.path(root, as.character(year), group)
   
+  if (!file.exists(group_dir)) {
+    cat("Skipping group:", group, " - Folder does not exist.\n")
+    next
+  }
+  
+  habitats <- list.files(group_dir)
+  
+  future_lapply(habitats, function(habitat) {
+    cat("Processing:", habitat, "\n")
+    habitat_dir <- file.path(group_dir, habitat)
+    
+    # Skip if no TIFF
+    tiff_files <- list.files(habitat_dir, pattern = "\\.tiff$", full.names = TRUE)
+    if (length(tiff_files) == 0) {
+      cat("Skipping habitat:", habitat, " - No mosaic tif found.\n")
+      return(NULL)
+    }
+    
+    raster <- terra::rast(tiff_files)
+    eu_mask <- terra::vect(eu_mask_path)
+    raster_masked <- terra::mask(raster, eu_mask)
+    
+    output_folder <- file.path(output_root, year, group)
+    if (!dir.exists(output_folder)) {
+      dir.create(output_folder, recursive = TRUE)
+      cat("Created folder:", output_folder, "\n")
+    }
+    
+    output_name <- file.path(output_folder, paste0(habitat, "_", year, "_connect_EU27.tiff"))
+    cat("Writing to:", output_name, "\n")
+    
+    terra::writeRaster(
+      raster_masked,
+      output_name,
+      overwrite = TRUE,
+      gdal = c("COMPRESS=NONE", "TFW=YES"),
+      datatype = 'INT1U'
+    )
+    
+    cat("Done processing:", basename(output_name), "\n")
+    return(output_name)
+  })
+}
